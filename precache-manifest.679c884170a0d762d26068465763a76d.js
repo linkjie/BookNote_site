40bf6fc1f6f4e23671030e0af1e289510edd08a6
linkj <413874345@qq.com>
2020-09-22 18:43:04 +0800

    deploy

---
+++ precache-manifest.679c884170a0d762d26068465763a76d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7258270647a339c9afb4",
+    "url": "/BookNote_site/css/app.b5b0f72f.css"
+  },
+  {
+    "revision": "6d0d69cd80e9de5de199",
+    "url": "/BookNote_site/css/chunk-0ec165a0.83b4c294.css"
+  },
+  {
+    "revision": "674c0b3aff49680aeea1",
+    "url": "/BookNote_site/css/chunk-394e211a.18e667eb.css"
+  },
+  {
+    "revision": "8ecb6a32c654a6461d23",
+    "url": "/BookNote_site/css/chunk-7547df2d.ff5c6543.css"
+  },
+  {
+    "revision": "f76a2519a26a1f253208",
+    "url": "/BookNote_site/css/chunk-bc3012ec.cd4e5d93.css"
+  },
+  {
+    "revision": "43834549aed4f3afc0f6",
+    "url": "/BookNote_site/css/chunk-c1c85ab8.ec25b623.css"
+  },
+  {
+    "revision": "981a38cc3a9faa07bb09",
+    "url": "/BookNote_site/css/chunk-vendors.b53b2ba6.css"
+  },
+  {
+    "revision": "43ab802256e3943037affb14f4336e64",
+    "url": "/BookNote_site/index.html"
+  },
+  {
+    "revision": "7258270647a339c9afb4",
+    "url": "/BookNote_site/js/app.95c20e65.js"
+  },
+  {
+    "revision": "6d0d69cd80e9de5de199",
+    "url": "/BookNote_site/js/chunk-0ec165a0.594a4c37.js"
+  },
+  {
+    "revision": "674c0b3aff49680aeea1",
+    "url": "/BookNote_site/js/chunk-394e211a.d34a9242.js"
+  },
+  {
+    "revision": "8ecb6a32c654a6461d23",
+    "url": "/BookNote_site/js/chunk-7547df2d.1ab33164.js"
+  },
+  {
+    "revision": "f76a2519a26a1f253208",
+    "url": "/BookNote_site/js/chunk-bc3012ec.16f7473c.js"
+  },
+  {
+    "revision": "43834549aed4f3afc0f6",
+    "url": "/BookNote_site/js/chunk-c1c85ab8.0f3a14bb.js"
+  },
+  {
+    "revision": "981a38cc3a9faa07bb09",
+    "url": "/BookNote_site/js/chunk-vendors.df2b272c.js"
+  },
+  {
+    "revision": "5b23cfa084ce86dc7c676499e6070c7e",
+    "url": "/BookNote_site/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BookNote_site/robots.txt"
+  }
+]);
